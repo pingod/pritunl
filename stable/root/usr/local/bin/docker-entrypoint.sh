d730f1b6aef7d741009326783ae52222186c1414
无常 <pingod@users.noreply.github.com>
2020-12-08 18:39:28 +0800

    Update docker-entrypoint.sh

--- stable/root/usr/local/bin/docker-entrypoint.sh
+++ stable/root/usr/local/bin/docker-entrypoint.sh
@@ -22,37 +22,6 @@ log "INFO - Script version ${SCRIPT_VERSION}"
 PRITUNL=/usr/bin/pritunl
 PRITUNL_OPTS="${PRITUNL_OPTS}"
 
-FAKE_API_CHOICE='yes'
-
-# 用于替换官方产品购买的API接口为第三方接口
-if [[ $FAKE_API_CHOICE == 'yes' ]];then
-  log "##################  Now Changing API Address to yours ##############"
-  export ORIG_API_SERVER='app.pritunl.com'
-  export ORIG_API_SERVER_ESCAPED=$(echo "$ORIG_API_SERVER" | sed -e 's/\./\\./g')
-  export ORIG_AUTH_SERVER='auth.pritunl.com'
-  export ORIG_AUTH_SERVER_ESCAPED=$(echo "$ORIG_AUTH_SERVER" | sed -e 's/\./\\./g')
-
-  export FAKE_API_SERVER='pritunl-api.simonmicro.de'
-  export FAKE_API_SERVER_ESCAPED=$(echo "$FAKE_API_SERVER" | sed -e 's/\./\\./g')
-  export FAKE_AUTH_SERVER="$FAKE_API_SERVER/auth/"
-  export FAKE_AUTH_SERVER_ESCAPED=$(echo "$FAKE_AUTH_SERVER" | sed -e 's/\./\\./g')
-
-  #find /usr/lib/python2.7 -type f -print0 | xargs -0 grep -o 'app.pritunl.com'
-  find /usr/lib/python2.7 -type f -print0 | xargs -0 sed -i "s/$ORIG_API_SERVER_ESCAPED/$FAKE_API_SERVER_ESCAPED/g"
-  #find /usr/lib/python2.7 -type f -print0 | xargs -0 grep -o 'pritunl-api.simonmicro.de'
-
-
-  find /usr/share/pritunl/www/ -type f -print0 | xargs -0 sed -i "s/$ORIG_API_SERVER_ESCAPED/$FAKE_API_SERVER_ESCAPED/g"
-  #find /usr/share/pritunl/www/ -type f -print0 | xargs -0 grep -o 'pritunl-api.simonmicro.de'
-
-  find /usr/lib/python2.7 -type f -print0 | xargs -0 sed -i "s#$ORIG_AUTH_SERVER_ESCAPED#$FAKE_AUTH_SERVER_ESCAPED#g"
-
-  find /usr/share/pritunl/www/ -type f -print0 | xargs -0 sed -i "s/$ORIG_AUTH_SERVER_ESCAPED/$FAKE_AUTH_SERVER_ESCAPED/g"
-elif [[ $FAKE_API_CHOICE == 'no' ]];then
-  log "##################  Skiped Chang API Address  ##############"
-fi
-
-
 pritunl_setup() {
     log "INFO - Insuring pritunl setup for container"
 
